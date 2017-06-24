6c208a567d0e6a96bc0a392d693ff67960c05315
nwdavis <nathanieldavis89@gmail.com>
2017-06-24 15:13:13 -0400

    adding files

---
+++ assets/game.js
@@ -0,0 +1,31 @@
+// Walter = 8 120
+// Gus = 25 180
+// Jesse = 5 100
+// Mike = 20 150
+
+//create objects for each character
+
+var walt {
+	hp: 120,
+	attack: 8,
+}
+
+var gus {
+	hp: 180,
+	attack: 25,
+}
+
+var jesse {
+	hp: 100,
+	attack: 5,
+}
+
+var mike {
+	hp: 150,
+	attack: 20,
+}
+
+$("#waltHp").html(walt.hp);
+$("#gusHp").html(gus.hp);
+$("#jesseHp").html(jesse.hp);
+$("#mikeHp").html(mike.hp);
